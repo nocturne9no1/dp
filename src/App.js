3483569063ef2cf72216d1eaebfef0f7f0487867
nocturne9no1 <nocturne9no1@gmail.com>
2023-03-24 02:52:49 +0900

    feat: chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Left from "./components/Left";
+import Right from "./components/Right";
+import "./style/_reset.scss";
+import "./style/common.scss";
 
 function App() {
+  const [startChat, setStartChat] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="radio-wrap">
+        <div className="radio-inner">
+          <Left setStartChat={setStartChat} />
+          <Right startChat={startChat} setStartChat={setStartChat} />
+        </div>
+      </div>
     </div>
   );
 }
