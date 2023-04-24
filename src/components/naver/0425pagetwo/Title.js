358056739f0cf4d89dabb16b7ef40162b0734c0f
edkim3275 <edkim3275@gmail.com>
2023-04-25 00:22:24 +0900

    feat : page2 완료

--- src/components/naver/0425pagetwo/Title.js
+++ src/components/naver/0425pagetwo/Title.js
@@ -1,4 +1,6 @@
 import SeowonLogo from "../../../assets/seowon-logo.svg";
+import Giza from "../../../assets/giza-image.jpeg";
+import IconList from "./IconList";
 
 const Title = () => {
   return (
@@ -10,13 +12,24 @@ const Title = () => {
           </div>
           <span>서원일보</span>
         </div>
-        <span>dfsfdss</span>
         <div className="right">
           <span>PiCK</span>
+          <div className="info">i</div>
         </div>
       </div>
-      <div>title</div>
-      <div>bugagineung</div>
+      <div className="title">
+        <p>지상파 아나운서 S씨, 촬영 중 난동 ‘충격’</p>
+        <p>입력2023.04.21. 오후 7:28</p>
+        <div className="author">
+          <div className="logo">
+            <img src={Giza} alt="" />
+          </div>
+          <span>한예슬 기자</span>
+        </div>
+      </div>
+      <div className="bugagineung bottom">
+        <IconList />
+      </div>
     </div>
   );
 };
--- src/pages/0425/PageTwo.js
+++ src/pages/0425/PageTwo.js
@@ -1,14 +1,13 @@
+import Contents from "../../components/naver/0425pagetwo/Contents";
 import Header from "../../components/naver/0425pagetwo/Header";
 import Title from "../../components/naver/0425pagetwo/Title";
 
-import IconList from "../../components/naver/0425pagetwo/IconList";
 const Page2 = () => {
   return (
     <div className="page2">
       <Header />
       <Title />
-      <IconList />
-      <div>content</div>
+      <Contents />
     </div>
   );
 };
