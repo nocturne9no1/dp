24fdf7f7b076ff52ec34ebc253bdb7afd8226172
nocturne9no1 <nocturne9no1@gmail.com>
2023-04-25 00:00:17 +0900

    feat: news icon row

--- src/pages/0425/PageTwo.js
+++ src/pages/0425/PageTwo.js
@@ -1,10 +1,13 @@
 import Header from "../../components/naver/0425pagetwo/Header";
 import Title from "../../components/naver/0425pagetwo/Title";
+
+import IconList from "../../components/naver/0425pagetwo/IconList";
 const Page2 = () => {
   return (
     <div className="page2">
       <Header />
       <Title />
+      <IconList />
       <div>content</div>
     </div>
   );
