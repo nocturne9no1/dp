876e7d703f703b1e14ec773593638072bb0ef7e5
nocturne9no1 <nocturne9no1@gmail.com>
2023-04-25 00:06:19 +0900

    feat: main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
+const linkList = [
+  {
+    title: "0421 - 뉴스 리스트 + 뉴스 1, 2, 3",
+    link: "/0421",
+  },
+  {
+    title: "0425 - 네이버 홈 뉴스 + 디테일",
+    link: "/0425/page-1",
+  },
+];
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="main-wrap">
+      <h1>소품 리스트</h1>
+      <ul>
+        {linkList.map((el, index) => (
+          <li key={el.title + index}>
+            <Link to={el.link}>{el.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Main;
