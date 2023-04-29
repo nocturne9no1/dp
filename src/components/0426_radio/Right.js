7ead1d6482d3f9ccccecf90a5471337c0d2ccb0f
nocturne9no1 <nocturne9no1@gmail.com>
2023-04-29 20:09:23 +0900

    feat: 라디오 수정 반영

--- src/components/0426_radio/Right.js
+++ src/components/0426_radio/Right.js
@@ -35,18 +35,18 @@ const type1 = [
     name: "rgegw",
     text: "ᄏᄏᄏᄏᄏᄏᄏ아그겈ᄏᄏᄏᄏ 제목이 뭐더라",
   },
-  {
-    name: "wrqrrrr",
-    text: "우포오오오오늪!!",
-  },
+  // {
+  //   name: "wrqrrrr",
+  //   text: "우포오오오오늪!!",
+  // },
   {
     name: "ewryours",
     text: "ᄏᄏᄏ앜ᄏᄏᄏᄏᄏ 그건 안됔ᄏᄏᄏᄏᄏ",
   },
-  {
-    name: "ghirtian",
-    text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ웃참챌린지 시작인가",
-  },
+  // {
+  //   name: "ghirtian",
+  //   text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ웃참챌린지 시작인가",
+  // },
   {
     name: "getaways",
     text: "오늘 정오 뉴스 방송사고 났던데 이것도 사고 내면 어캄ᄏᄏᄏᄏᄏᄏ",
@@ -59,26 +59,26 @@ const type1 = [
     name: "says_er",
     text: "에이~~ 프로잖아 프로!! 할수이따 송기백!!",
   },
-  {
-    name: "debased",
-    text: "김바다 시인의 우포늪!!! 신청합니닷!!! 가즈아아ᅡᅡᅡ!!!",
-  },
-  {
-    name: "fellwwer",
-    text: "자아~~ 새들의 세상으로 빠져~~봅시다~~",
-  },
+  // {
+  //   name: "debased",
+  //   text: "김바다 시인의 우포늪!!! 신청합니닷!!! 가즈아아ᅡᅡᅡ!!!",
+  // },
+  // {
+  //   name: "fellwwer",
+  //   text: "자아~~ 새들의 세상으로 빠져~~봅시다~~",
+  // },
   {
     name: "Sdhrwls",
     text: "???",
   },
-  {
-    name: "Pshky",
-    text: "아.. 벌써 웃기다",
-  },
-  {
-    name: "Sho_ch",
-    text: "이거 아침 알람으로 딱인듯 ᄏᄏᄏᄏ",
-  },
+  // {
+  //   name: "Pshky",
+  //   text: "아.. 벌써 웃기다",
+  // },
+  // {
+  //   name: "Sho_ch",
+  //   text: "이거 아침 알람으로 딱인듯 ᄏᄏᄏᄏ",
+  // },
   {
     name: "cdusalw",
     text: "우리집 강아지가 막 짓어요...ᅮᅮ",
@@ -162,6 +162,134 @@ const type1 = [
   {
     name: "ghazjald",
     text: "오랜만에 들으러 왔는데 밍디가 아니네",
+  },
+  {
+    name: "abcefa",
+    text: "기백씨 안녕하세요^^",
+  },
+  {
+    name: "dduyw22",
+    text: "오! 뭔가 낯설다",
+  },
+  {
+    name: "hoqwe",
+    text: "누구라고??",
+  },
+  {
+    name: "qazplm45",
+    text: "헐 형 팬이에요",
+  },
+  {
+    name: "Sktlrhfod",
+    text: "찐임?",
+  },
+  {
+    name: "food984",
+    text: "아까 콧구멍 사고 난 애 아님?",
+  },
+  {
+    name: "aldks",
+    text: "누구라고? 자기 소개 한번만 더 해줘요~~~",
+  },
+  {
+    name: "oouu33",
+    text: "밍디는??",
+  },
+  {
+    name: "thznFl",
+    text: "ᄏᄏᄏᄏᄏ콧구멍ᄏᄏᄏ",
+  },
+  {
+    name: "sksmswndus",
+    text: "시읽남 밍디에서 바뀐거임?",
+  },
+  {
+    name: "집이야",
+    text: "날이 좋아^^ 분갈이 하고 있어요~ 어울리는 시 읽 어주세요~~",
+  },
+  {
+    name: "피곤해9281",
+    text: "밍디 무슨일 있나?",
+  },
+  {
+    name: "kuyu68",
+    text: "밍디가 읽어주는 시 듣고 싶었는데 ᅲᅲ",
+  },
+  {
+    name: "rhfharh",
+    text: "아나운서가 읽어주는 시라니^^",
+  },
+  {
+    name: "qjtmdkszz",
+    text: "내가 아는 송기백임?",
+  },
+  {
+    name: "wkrhtlvdj",
+    text: "나는 멋있던데...? 콧구멍 빼고ᄏᄏᄏᄏ",
+  },
+  {
+    name: "sjsmswndu",
+    text: "제가 쓴 시 읽어주세요!!!",
+  },
+  {
+    name: "tjsgus",
+    text: "오라버니 안녕하세요♡",
+  },
+  {
+    name: "dfaas34",
+    text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ기사 엄청 났던데",
+  },
+  {
+    name: "dfioi91",
+    text: "정오뉴스 송기백?",
+  },
+  {
+    name: "rkvudrns",
+    text: "나 김성훈 팬이었는데...ᅲᅲᅲᅲᅲᅲ",
+  },
+  {
+    name: "sunghoon1004",
+    text: "무슨일 있었음??",
+  },
+  {
+    name: "ooui38",
+    text: "ᄏᄏᄏᄏᄏᄏ방송사고 땜에 ᅲᅲ집중 안돼요..",
+  },
+  {
+    name: "menmen",
+    text: "오늘 무슨 날임?",
+  },
+  {
+    name: "qnrausrkwk",
+    text: "형 뉴스는 어떻게 됐어요",
+  },
+  {
+    name: "wkalos2002",
+    text: "으으..!",
+  },
+  {
+    name: "dfpop77",
+    text: "다음주는 밍디 오나요?",
+  },
+  {
+    name: "qhrhtlvdj",
+    text: "저녁 뭐 먹을지 메뉴 추천해주세요.",
+  },
+  {
+    name: "khss1019",
+    text: "울엄마 회원가입 해드렸어요ᄒᄒ 앞으로 많이 읽어 주세요.",
+  },
+  {
+    name: "jqyoung20",
+    text: "일하면서 잘듣고 있어요",
+  },
+  {
+    name: "soundy98",
+    text: "피디님 선곡? 선시? 아무튼 최고네요!!",
+  },
+  {
+    name: "lhjsl2",
+    text: "ᄒᄋ",
   },
 ];
 
@@ -302,17 +430,149 @@ const type2 = [
     name: "aksgrhksqn",
     text: "프브스 선정 '우포늪 제일 잘 읽는 사람 1위'",
   },
+  {
+    name: "jugg",
+    text: "ᄏᄏᄏᄏᄏᄏ역시 아나운서네",
+  },
+  {
+    name: "thansemerh",
+    text: "소문듣고 찾아왔슴다^^7",
+  },
+  {
+    name: "ekfekfdl",
+    text: "ᄏᄏᄏᄏᄏᄏᄏᄏ송기백 뭐야 ᄏᄏᄏᄏ웃기네",
+  },
+  {
+    name: "kes4023",
+    text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ밍디 밥그릇 조심해야할 듯",
+  },
+  {
+    name: "toyou",
+    text: "정오뉴스도 그렇고 ᄏᄏᄏᄏ오늘 송기백데이인 듯",
+  },
+  {
+    name: "jjunem8584",
+    text: "^^;(",
+  },
+  {
+    name: "tlsrlfn2002",
+    text: "눈 앞에 우포늪이 펼쳐짐.",
+  },
+  {
+    name: "happy65",
+    text: "기백씨 다음에 또 나와줘요~ 우리 딸이 같이 듣고 너무 좋아하네요^^",
+  },
+  {
+    name: "o8896o",
+    text: "ᄏᄏᄏᄏᄏᄏᄏ라디오 하나 해도 될 듯",
+  },
+  {
+    name: "ititt88",
+    text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ골때린다",
+  },
+  {
+    name: "dfhur",
+    text: "와 이걸 이렇게 읽는다고?",
+  },
+  {
+    name: "bbyoii23",
+    text: "뭐야 나 못들었어ᅲ 다시 읽어줘 ᅲᅮ",
+  },
+  {
+    name: "ekdmams",
+    text: "다음은 무슨 시? 기대되는데ᄏᄏᄏᄏᄏ",
+  },
+  {
+    name: "aajdej81",
+    text: "밍디 불안할 듯ᄏᄏᄏᄏᄏᄏ",
+  },
+  {
+    name: "tlrksdk",
+    text: "지금 밍디 뛰어오는 중임;;",
+  },
+  {
+    name: "douiq190",
+    text: "원래 이런 시임? ᄏᄏᄏᄏᄏᄏ",
+  },
+  {
+    name: "tlrksdk",
+    text: "지금 밍디 뛰어오는 중임;;",
+  },
+  {
+    name: "claeochlrh",
+    text: "다들 뭐해? 송기백 안쓰고?",
+  },
+  {
+    name: "lmaroll",
+    text: "안녕하세요~",
+  },
+  {
+    name: "ziin0218",
+    text: "뼈 때렸다.",
+  },
+  {
+    name: "dlqnftkffo88",
+    text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ",
+  },
+  {
+    name: "akfkzkd21",
+    text: "와.. 발음 정확한 거 보소",
+  },
+  {
+    name: "vkdlxld",
+    text: "아나운서는 아나운서네ᄏᄏᄏᄏ",
+  },
+  {
+    name: "bbyoii23",
+    text: "뭔데.. 나 못들었다고.. 왜 다들 뭇는데..",
+  },
+  {
+    name: "rkdgkssk11",
+    text: "시 읽기의 정석이네요^^",
+  },
+  {
+    name: "realove82",
+    text: "오빠 수고 많았어요. 그냥 낼 만나요",
+  },
+  {
+    name: "c54103285",
+    text: "정신이 사나워 꿈자리 뒤숭숭 할 듯.",
+  },
+  {
+    name: "22sung",
+    text: "그래서 밍디 언제 온다고?",
+  },
+  {
+    name: "chosim45",
+    text: "ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ뉴스 기사 찾아봐야겠네",
+  },
+  {
+    name: "parkhm111",
+    text: "ᅮᅮᅮᅮ자꾸 귀에서 맴돌아",
+  },
+  {
+    name: "kji5060",
+    text: "나 꼭 다시듣기로 들을거야ᄏᄏᄏᄏᄏ",
+  },
+  {
+    name: "09skysea",
+    text: "ᄏᄏᄏᄏᄏᄏᄏ오포늪 1시간 재생 가자!!",
+  },
+  {
+    name: "lsuk4145",
+    text: "시가 기가 막히네 아주ᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏᄏ",
+  },
 ];
 
 const Right = ({ startChat, setStartChat, type }) => {
   const Dummy = type === 1 ? type1 : type2;
   const [chatList, setChatList] = useState([]);
+  let idx = 4;
   useEffect(() => {
     if (startChat) {
-      let idx = 0;
       const intervalId = setInterval(() => {
-        console.log(idx, Dummy.length);
-        console.log(Dummy[idx]);
+        // console.log(idx, Dummy.length);
+        // console.log(Dummy[idx]);
         // 이거 왜 -1 일까...?
         if (idx < Dummy.length - 1) {
           setChatList((prev) => [Dummy[idx], ...prev]);
@@ -321,6 +581,10 @@ const Right = ({ startChat, setStartChat, type }) => {
           setStartChat(false);
           clearInterval(intervalId);
         }
+        if (idx === Dummy.length - 1) {
+          idx = 0;
+        }
+        console.log(idx, chatList.length);
       }, 500);
       return () => clearInterval(intervalId);
     }
@@ -354,6 +618,18 @@ const Right = ({ startChat, setStartChat, type }) => {
         <div className="chat-list-wrap">
           <ul className="chat-list">
             {chatList.map(
+              (el, index) =>
+                el.name &&
+                el.text && (
+                  <ChatBox
+                    key={el.name + index}
+                    name={el.name}
+                    text={el.text}
+                    sec={1}
+                  />
+                )
+            )}
+            {Dummy.slice(0, 4).map(
               (el, index) =>
                 el.name &&
                 el.text && (
